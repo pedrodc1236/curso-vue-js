3321afd0930a82a0d57269c43fc092d52643008c
Pedro Tavares <pedrodc1236@gmail.com>
2022-12-03 20:28:40 -0300

    desafio 05 realizado

---
+++ 03-usando-condicionais-e-renderizacao-de-listas/condicionais-e-listas/desafio-05/app.js
@@ -0,0 +1,29 @@
+new Vue({
+	el: '#desafio',
+	data: {
+		array: ['Pedro', 'Bia', 'Ana', 'Rebeca'],
+		livro: {
+			titulo: 'O Senhor dos Anéis',
+			autor: 'J.R.R. Tolkiens',
+			volume: '3'
+		},
+		aluno: {
+			id: 10,
+			nome: 'Maria',
+			notas: [7.67, 8.33, 6.98, 9.21]
+		},
+		alterar: true,
+		animes: [
+			{ nome: 'One Piece', episodios: 1037 },
+			{ nome: 'FullMetal Alchemist Brotherhood', episodios: 64 },
+			{ nome: 'Hunter x Hunter', episodios: 148 },
+			{ nome: 'Naruto', episodios: 500 },
+			{ nome: 'Yuyu Hakusho', episodios: 112 }
+		]
+	},
+	// methods: {
+	// 	alterarFunc() {
+	// 		this.
+	// 	}
+	// }
+});
