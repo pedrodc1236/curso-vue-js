59ec1e4d11412be3ab91bea5b71738580f4affda
Pedro Tavares <pedrodc1236@gmail.com>
2022-12-20 13:50:20 -0300

    criando um componente reutilizavel

---
+++ 06-introducao-aos-componentes/componente-intro/src/Contador.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="contador">
+      <span>{{ contador }}</span>
+      <button @click="add">+</button>
+      <button @click="sub">-</button>
+  </div>
+</template>
+
+
+<script>
+export default {
+  data() {
+    return {
+      contador: 0
+    }
+  },
+  methods: {
+      add() {
+          this.contador++
+      },
+      sub() {
+          this.contador--
+      }
+  }
+}
+</script>
+
+<style>
+  .contador span {
+      border-bottom: 2px solid rgba(158, 9, 9, 0.322);
+      height: 20px;
+      padding: 4px 15px;
+  }
+
+  .contador button {
+    height: 26px;
+    width: 26px;
+    border-radius: 13px;
+    background-color: purple;
+    color: white;
+  }
+</style>
