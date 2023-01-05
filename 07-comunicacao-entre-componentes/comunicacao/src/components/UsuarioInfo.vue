6f6b50afd0d0144f8f1a660181cf57ad6c3946c8
Pedro Tavares <pedrodc1236@gmail.com>
2023-01-05 16:21:26 -0300

    usando event bus para comunicação entre componentes irmãos

--- 07-comunicacao-entre-componentes/comunicacao/src/components/UsuarioInfo.vue
+++ 07-comunicacao-entre-componentes/comunicacao/src/components/UsuarioInfo.vue
@@ -10,6 +10,8 @@
 </template>
 
 <script>
+import barramento from '@/barramento'
+
 export default {
     props: {
         nome: {
@@ -28,6 +30,11 @@ export default {
             this.nome = 'Pedro',
             this.$emit('mudouNome', this.nome)
         }
+    },
+    created() {
+        barramento.$on('idadeMudou', idade => {
+            this.idade = idade
+        })
     }
 }
 </script>
